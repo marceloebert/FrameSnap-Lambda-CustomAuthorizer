1198c188098e1044192a1ceca2e2717f8cfe3867
Marcelo Ebert <ebert.marcelo@gmail.com>
2025-04-14 21:40:07 -0300

    Add new SVC K8S

--- api-gateway/variables.tf
+++ api-gateway/variables.tf
@@ -5,12 +5,12 @@ variable "region" {
 
 variable "eks_service_endpoint" {
   description = "EKS public endpoint (sem /videos no final)"
-  default     = "http://a65f7d678ccd0469ba97728784513106-1518254404.us-east-1.elb.amazonaws.com/videos"
+  default     = "http://a8e3cd4e5f0b349dd8d4c43539afbca8-39016098.us-east-1.elb.amazonaws.com/videos"
 }
 
 variable "login_service_endpoint" {
   description = "EKS login service endpoint (rota /login)"
-  default     = "http://aac02e3ca3283499a9a6f48fa119442d-1681479322.us-east-1.elb.amazonaws.com/auth"
+  default     = "http://a8ab722c3555440bea881a4fa65463d5-1866891439.us-east-1.elb.amazonaws.com/auth"
 }
 
 variable "lambda_function_name" {
